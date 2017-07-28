aa9d6aa94b06ebac7a734b687ea4955a2580e5a7
Barry Song <21cnbao@gmail.com>
2017-07-28 23:36:18 +0800

    uboot to single core and use 1235 port for gdbserver
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- extra/run-nolcd.sh
+++ extra/run-nolcd.sh
@@ -1,2 +1,2 @@
-qemu-system-arm -nographic -sd vexpress.img -M vexpress-a9 -m 512M -kernel zImage -dtb vexpress-v2p-ca9.dtb  -smp 4 -append "init=/linuxrc root=/dev/mmcblk0p1 rw rootwait earlyprintk console=ttyAMA0 initcall_debug" -redir tcp:1234::1234 2>/dev/null
+qemu-system-arm -nographic -sd vexpress.img -M vexpress-a9 -m 512M -kernel zImage -dtb vexpress-v2p-ca9.dtb  -smp 4 -append "init=/linuxrc root=/dev/mmcblk0p1 rw rootwait earlyprintk console=ttyAMA0 initcall_debug" -redir tcp:1235::1235 2>/dev/null
 
--- extra/u-boot.sh
+++ extra/u-boot.sh
@@ -1,1 +1,1 @@
-qemu-system-arm -smp 2 -sd vexpress.img -M vexpress-a15 -serial stdio -kernel u-boot
+qemu-system-arm -smp 1 -sd vexpress.img -M vexpress-a15 -m 1024M -serial stdio -kernel u-boot
