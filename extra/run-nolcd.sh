fbb87c43885d815f8ad0cc022ca0cee503241060
Barry Song <21cnbao@gmail.com>
2019-12-18 19:17:19 +0800

    some debug code
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- extra/run-nolcd.sh
+++ extra/run-nolcd.sh
@@ -1,2 +1,2 @@
-qemu-system-arm -nographic -sd vexpress.img -M vexpress-a9 -m 512M -kernel zImage -dtb vexpress-v2p-ca9.dtb  -smp 4 -append "init=/linuxrc root=/dev/mmcblk0p1 rw rootwait earlyprintk console=ttyAMA0 initcall_debug" -redir tcp:1235::1235 2>/dev/null
+qemu-system-arm  -nographic -sd vexpress.img -M vexpress-a9 -m 512M -kernel zImage -dtb vexpress-v2p-ca9.dtb  -smp 4 -append "init=/linuxrc slub_debug root=/dev/mmcblk0p1 rw rootwait log_level=8 console=ttyAMA0" -redir tcp:1235::1235 -redir udp:5001::5001 2>/dev/null
 
